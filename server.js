155f21827203eafca707eb6a2c11c09234ccacbb
Jyoti Prajapati <94469107+Rusta007@users.noreply.github.com>
2024-11-28 02:59:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,11 @@ let students = [
     }
 ]
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next();
+})
+
 app.get('/', (req, res) => {
     console.log("from server");
     res.sendStatus(404)
